b26e095fd59c1363c73109d9f6aee66f4af7aee7
radunicula <radu7.nicula@gmail.com>
2022-01-28 21:43:19 +0200

    add function for generate buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var x = document.createElement("BUTTON");
+    var t = document.createTextNode("Click me");
+    x.appendChild(t);
+    document.body.appendChild(x);
+  }
+  
