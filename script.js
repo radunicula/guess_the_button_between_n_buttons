3966ad7dd779c4374cd02d890f7505c1fc0d101b
radunicula <radu7.nicula@gmail.com>
2022-01-30 09:41:58 +0200

    final version
    moved script from index to script

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function myFunction() {
-    var x = document.createElement("BUTTON");
-    var t = document.createTextNode("Click me");
-    x.appendChild(t);
-    document.body.appendChild(x);
+function Geeks() {
+  var number_of_buttons = document.getElementById("number_of_buttons").value;
+  var x = Math.round(Math.random() * number_of_buttons);
+  for (var i = 0; i < number_of_buttons; i++) {
+    var myDiv = document.getElementById("GFG");
+    var button = document.createElement('BUTTON');
+    var text = document.createTextNode(i + 1);
+    button.appendChild(text);
+    button.classList.add("btn");
+    button.classList.add("btn-success");
+    myDiv.appendChild(button);
+    if (i == x) {
+      button.setAttribute('onclick', 'win(); disable()')
+    } else {
+      button.setAttribute('onclick', 'lost(); disable()')
+    }
   }
+}
+function disable_ok() {
+  var btn_ok = document.getElementById("btn_ok");
+  btn_ok.disabled = true;
+}
+function disable() {
+  var elems = document.getElementsByClassName("btn btn-success");
+  for (var i = 0; i < elems.length; i++) {
+    elems[i].disabled = true;
+  }
+}
+function lost() {
+  document.getElementById('result').style.color = "red";
+  document.getElementById('result').innerHTML = "you lost!";
+}
+function win() {
+  document.getElementById('result').style.color = "green";
+  document.getElementById('result').innerHTML = "you win!";
+}
